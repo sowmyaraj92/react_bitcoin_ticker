958b531dd3cb01052e962822e73738915c130da3
Gagan Sikri <sikrigagan@sikrigagan-MacBook-Pro.local>
2018-12-16 23:59:59 +0530

    Added build files

---
+++ precache-manifest.57ed66fc3b929dceb2689e57066a6c8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d991e3d3bfa25810a9ef",
+    "url": "/static/js/main.d991e3d3.chunk.js"
+  },
+  {
+    "revision": "8b61350c16572aceea07",
+    "url": "/static/js/1.8b61350c.chunk.js"
+  },
+  {
+    "revision": "d991e3d3bfa25810a9ef",
+    "url": "/static/css/main.07ad4e7b.chunk.css"
+  },
+  {
+    "revision": "8b61350c16572aceea07",
+    "url": "/static/css/1.dfe54033.chunk.css"
+  },
+  {
+    "revision": "37e3e92ad14ddcefabe3c2fefbf1dc68",
+    "url": "/index.html"
+  }
+];
